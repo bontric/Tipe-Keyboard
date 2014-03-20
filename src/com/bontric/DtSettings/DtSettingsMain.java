1b4c64d46e8f338e789fb3ea295c580941623361
ben <b.wieder@fu-berlin.de>
2014-03-20 02:12:52 +0100

    fixed messy code

---
+++ src/com/bontric/DtSettings/DtSettingsMain.java
@@ -0,0 +1,22 @@
+package com.bontric.DtSettings;
+
+import com.bontric.DoubleTab.R;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+@SuppressWarnings("deprecation")
+public class DtSettingsMain extends PreferenceActivity {
+
+	public final static String bWS = "startWithShift";
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+	}
+
+
+
+}
