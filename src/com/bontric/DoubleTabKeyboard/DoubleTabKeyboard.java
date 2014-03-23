2a86b53818a317d0ee1edc0f9b3fa8a0133fbe7b
ben <b.wieder@fu-berlin.de>
2014-03-23 20:39:50 +0100

    added slider, no implementation yet (got lib from https://github.com/jayschwa/AndroidSliderPreference)

--- src/com/bontric/DoubleTabKeyboard/DoubleTabKeyboard.java
+++ src/com/bontric/DoubleTabKeyboard/DoubleTabKeyboard.java
@@ -81,7 +81,7 @@ public class DoubleTabKeyboard extends Keyboard {
 
 		@Override
 		public boolean isInside(int x, int y) {
-			return super.isInside(x, y+20);
+			return super.isInside(x, y);
 			// this is bad but important.. TODO ensure to find a nice fix one
 			// day!
 		}
